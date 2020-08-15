d589a739be90ba08e1f74cb4e6923cf6f5977976
Deependra rout <deependrakumarrout@gmail.com>
2020-08-16 00:58:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/andoy/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.andoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.leo.simplearcloader.ArcConfiguration;
+import com.leo.simplearcloader.SimpleArcDialog;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void actionPerformed(View view) {
+        SimpleArcDialog mDialog = new SimpleArcDialog(this);
+        mDialog.setConfiguration(new ArcConfiguration(this));
+        mDialog.setTitle("Loading...");
+        mDialog.show();
+
+
+    }
+
+}
